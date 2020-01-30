beaf44df4dea88d3c881425cf0d57ad92a5b0f95
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2020-01-30 12:11:42 +0100

    Fixed the puphpet config

--- html/module/Application/src/Controller/IndexController.php
+++ html/module/Application/src/Controller/IndexController.php
@@ -97,7 +97,7 @@ class IndexController extends AbstractActionController
                     move_uploaded_file($data['image-file']['tmp_name'], $path);
 
                     // Search for the image
-                    $similarImages = $this->imageRepository->getImagesInDistance($path, 20);
+                    $similarImages = $this->imageRepository->getImagesInDistance($path, 10);
 
                     unlink($path);
 
